1a43248296e26cf6bcd4591fda33369e17bc2d2d
Derryck <oseiagyemanb@gmail.com>
2023-03-24 06:29:04 +0100

    initial commit of file

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+   const [showAlert, setShowAlert] = useState(false);
+
+   // const toggleAlert = () => {
+   //    if (showAlert) {
+   //       setShowAlert(false);
+   //       return;
+   //    }
+   //    setShowAlert(true);
+   // };
+
+   return (
+      <div>
+         <button
+            onClick={() => setShowAlert(!showAlert)}
+            className="btn"
+            style={{ background: 'violet' }}
+         >
+            Progress
+         </button>
+         {showAlert && <Alert />}
+      </div>
+   );
+};
+
+const Alert = () => {
+   return (
+      <div className="alert alert-danger" style={{ background: '#8ff75f' }}>
+         IPO Launching progress...
+      </div>
+   );
 };
 
 export default ToggleChallenge;
