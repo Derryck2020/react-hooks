6e9470edcee171088d78358c4df47dce7e13df2c
Derryck <oseiagyemanb@gmail.com>
2023-03-25 12:14:35 +0100

    update of App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Starter from './tutorial/02-useEffect/starter/04-fetch-data';
+import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+import Final from './tutorial/02-useEffect/final/05-cleanup-function';
 import './App.css';
 function App() {
    return (
       <div className="container">
          <Starter />
+         {/* <Final /> */}
       </div>
    );
 }
